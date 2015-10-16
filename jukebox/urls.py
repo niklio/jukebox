407abd13cde45d43606305f837e7a7210d17c327
Nikolas Liolios <Nliolios@exeter.edu>
2015-10-16 03:39:13 -0400

    changed authentication URIs

--- jukebox/urls.py
+++ jukebox/urls.py
@@ -5,10 +5,6 @@ urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^auth/login', 'rest_framework_jwt.views.obtain_jwt_token'),
-    url(r'^auth/refresh', 'rest_framework_jwt.views.refresh_jwt_token'),
-    url(r'^auth/verify', 'rest_framework_jwt.views.verify_jwt_token'),
-
     # APIs
     url(r'^api/users/', include('users.urls')),
     url(r'^api/pods/', include('pods.urls')),
--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,11 @@ from users import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', views.UserProfileList.as_view()),
+
     url(r'^register', 'users.views.register'),
+    url(r'^login', 'rest_framework_jwt.views.obtain_jwt_token'),
+    url(r'^token/refresh', 'rest_framework_jwt.views.refresh_jwt_token'),
+    url(r'^token/verify', 'rest_framework_jwt.views.verify_jwt_token'),
+
+    url(r'^$', views.UserProfileList.as_view()),
 )
