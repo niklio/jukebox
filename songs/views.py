4a6f401da88bf2ecbf8a24bb3129e587d52c6da9
Nikolas Liolios <Nliolios@exeter.edu>
2015-10-28 16:38:11 -0400

    Added fields to serializers and Basic song viewset

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import permissions, viewsets, status
+from rest_framework.response import Response
+
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
+from songs.models import Song
+from songs.serializers import SongSerializer
+
+
+class SongViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows songs to be viewed or edited.
+    """
+    lookup_field = 'id'
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (JSONWebTokenAuthentication,)
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
