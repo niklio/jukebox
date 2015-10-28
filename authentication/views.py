b557e2779149a6eb1952aac07509bb8f8fa1dd3d
Nikolas Liolios <Nliolios@exeter.edu>
2015-10-28 17:38:30 -0400

    Adding songs to pods works

--- authentication/views.py
+++ authentication/views.py
@@ -28,3 +28,13 @@ class AccountViewSet(viewsets.ModelViewSet):
             return (permissions.AllowAny(),)
 
         return (permissions.IsAuthenticated(), IsAccountOwner())
+
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            account = Account.objects.create_user(**serializer.validated_data)
+
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+        return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST)
--- songs/views.py
+++ songs/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
+from authentication.models import Account
+from pods.models import Pod
 from songs.models import Song
 from songs.serializers import SongSerializer
 
@@ -18,3 +20,35 @@ class SongViewSet(viewsets.ModelViewSet):
     authentication_classes = (JSONWebTokenAuthentication,)
     queryset = Song.objects.all()
     serializer_class = SongSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        account = Account.objects.get(username=serializer.validated_data['submitted_by'])
+        pod = Pod.objects.get(name=serializer.validated_data['pod'])
+
+        if account.username != request.user.username:
+            return Response({
+                'status': 'Forbidden',
+                'message': 'You do not have permission to submit a song as user: {}.'.format(account.username)
+            }, status=status.HTTP_403_FORBIDDEN)
+
+        if not account.pod or account.pod.name != pod.name:
+            return Response({
+                'status': 'Forbidden',
+                'message': 'You do not have permission to submit a song to pod: {}'.format(pod.name)
+            }, status=status.HTTP_403_FORBIDDEN)
+
+        serializer.save()
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
+
