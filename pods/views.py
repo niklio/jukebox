647326683eba4961839f251291000ad23c4d0002
Nikolas Liolios <Nliolios@exeter.edu>
2015-10-28 10:19:29 -0400

    Switched to name based api. Users can only create pods hosted by them

--- pods/views.py
+++ pods/views.py
@@ -13,17 +13,27 @@ class PodViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows pods to be viewed or edited.
     """
+    lookup_field = 'name'
     permission_classes = (permissions.IsAuthenticated,)
     authentication_classes = (JSONWebTokenAuthentication,)
     queryset = Pod.objects.all()
     serializer_class = PodSerializer
 
-    def create(self, request):
-        request.data.update({'host': request.user.id})
-        serializer = self.serializer_class(data = request.data)
 
-        print serializer.is_valid()
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST)
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if not serializer.is_valid():
+            return Response({
+                'status': 'Bad request',
+                'message': 'Pod could not be created with received data'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        if serializer.data['host'] != request.user.username:
+            return Response({
+                'status': 'Unauthorized',
+                'message': 'Not authorized to create a pod hosted by {}. Check your authentication header'.format(serializer.data['host'])
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
